c71a1e839199e1c04dd9cc37760ba579af94241f
Jalope <60742112+Jalope@users.noreply.github.com>
2022-04-04 22:16:12 +0200

    act

--- Tres_en_raya/game_final.py
+++ Tres_en_raya/game_final.py
@@ -1,6 +1,4 @@
-#tablero1 = [[1,1,-1],[-1,1,1],[-1,1,1]]
-#tablero2 = [[1,1,-1],[-1,-1,1],[-1,1,1]]
-
+import numpy as np
 
 #LOGICA DEL TRES EN RAYA
 
@@ -60,3 +58,104 @@ def comprobar_diagonal(tablero, condicion):
 #b1 = comprobar_diagonal(tablero1, 3)
 #b2 = comprobar_diagonal(tablero2, -3)
 #print(b1, b2)
+
+def jugada_P1(tablero): 
+    fila = int(input("Introduce la fila que quieres marcar: "))
+    columna = int(input("Introduce la columna que quieres marcar: "))
+    jugada = (fila, columna)
+    repetir = True 
+    
+    while repetir: 
+        aux = "Marcaras la casilla: ", jugada, ".", "¿Estas seguro? (S/N)"
+        respuesta = str(input(aux)) 
+        respuesta = respuesta.upper()
+        if respuesta == "N":
+            fila = int(input("Introduce la fila que quieres marcar: "))
+            columna = int(input("Introduce la columna que quieres marcar: "))
+            jugada = (fila, columna)
+            repetir = True
+        else: 
+            repetir = comprobar_jugada(tablero, jugada)    
+
+    if repetir == False: 
+        tablero[jugada[0]][jugada[1]] = 1
+
+    print(tablero)
+
+    return jugada 
+
+def jugada_P2(tablero): 
+    fila = int(input("Introduce la fila que quieres marcar: "))
+    columna = int(input("Introduce la columna que quieres marcar: "))
+    jugada = (fila, columna)
+    repetir = True 
+    
+    while repetir: 
+        aux = "Marcaras la casilla: ", jugada, ".", "¿Estas seguro? (S/N)"
+        respuesta = str(input(aux)) 
+        respuesta = respuesta.upper()
+        if respuesta == "N":
+            fila = int(input("Introduce la fila que quieres marcar: "))
+            columna = int(input("Introduce la columna que quieres marcar: "))
+            jugada = (fila, columna)
+            repetir = True
+        else: 
+            repetir = comprobar_jugada(tablero, jugada)    
+
+    if repetir == False: 
+        tablero[jugada[0]][jugada[1]] = -1
+
+    print(tablero)
+
+    return jugada 
+
+def comprobar_jugada(tablero, jugada): 
+    if tablero[jugada[0]][jugada[1]] == 0: 
+        print("Jugada válida")
+        return False
+    else: 
+        print("Jugada no válida")
+        return True
+
+def comprobar_game_over(tablero, condicion):
+    a = comprobar_fila(tablero, condicion)
+    b = comprobar_columna(tablero, condicion)
+    c = comprobar_diagonal(tablero, condicion)
+
+    if (a == True) or (b == True) or (c == True): 
+        return True 
+    else:
+        return False 
+
+
+def main():
+    running = True 
+    tablero = np.zeros((3,3))
+    turno = 1
+    game_over = False
+
+    while running: 
+        if turno == 1: 
+            jugada_P1(tablero)
+            game_over = comprobar_game_over(tablero, 3)
+            if game_over == True: 
+                print("Ha ganado el Jugador 1")
+                break 
+            else: 
+                print(game_over)
+                turno = -1
+                print("CAMBIO DE TURNO")
+        else: 
+            jugada_P2(tablero)
+            game_over = comprobar_game_over(tablero, -3)
+            if game_over == True: 
+                running == False
+                print("Ha ganado el Jugador 2")
+                break
+            else: 
+                turno = 1
+                print("CAMBIO DE TURNO")
+
+
+if __name__ == "__main__": 
+    main()
